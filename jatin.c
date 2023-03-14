e92e19d421e2b2e888439c5093b487fb4876283f
Jatingahlyan07 <122517841+Jatingahlyan07@users.noreply.github.com>
2023-03-14 11:58:41 +0530

    Add files via upload

---
+++ jatin.c
@@ -0,0 +1,24 @@
+class ATM:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def check_balance(self):
+        print("Your account balance is ${:.2f}".format(self.balance))
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("${:.2f} has been deposited into your account. Your new balance is ${:.2f}".format(amount, self.balance))
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount
+            print("${:.2f} has been withdrawn from your account. Your new balance is ${:.2f}".format(amount, self.balance))
+
+# example usage
+my_atm = ATM(1000) # create an ATM object with an initial balance of $1000
+my_atm.check_balance() # prints "Your account balance is $1000.00"
+my_atm.deposit(500) # prints "$500.00 has been deposited into your account. Your new balance is $1500.00"
+my_atm.withdraw(200) # prints "$200.00 has been withdrawn from your account. Your new balance is $1300.00"
+my_atm.withdraw(1500) # prints "Insufficient funds."
